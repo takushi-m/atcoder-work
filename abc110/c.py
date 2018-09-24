ff63cf780a1768e99b480807efea5df4091bb15e
MIKI Takushi <takushi-m@users.noreply.github.com>
2018-09-24 13:58:02 +0900

    work

--- abc110/c.py
+++ abc110/c.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+
 def func(s,t):
-    if s==t:
-        return "Yes"
-    elif len(size(s))>len(size(t)):
-        return "No"
-
-    
-
-
-    pass
+    n = len(s)
+    for c in set(s):
+        d = None
+        for i in range(n):
+            if s[i]==c:
+                if d is None:
+                    d = t[i]
+                elif d!=t[i]:
+                    return "No"
+    for c in set(t):
+        d = None
+        for i in range(n):
+            if t[i]==c:
+                if d is None:
+                    d = s[i]
+                elif d!=s[i]:
+                    return "No"
+    return "Yes"
 
 class Test(unittest.TestCase):
     def test(self):
@@ -20,6 +30,7 @@ class Test(unittest.TestCase):
         self.assertEqual(func("abc", "abc"), "Yes")
 
 if __name__ == '__main__':
-    # s = input()
-    # t = input()
-    unittest.main()
+    s = input()
+    t = input()
+    # unittest.main()
+    print(func(s,t))
