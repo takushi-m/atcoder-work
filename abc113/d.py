4614e0f2cbeb962a2cb2d105c9ccc53d7770fdcc
MIKI Takushi <takushi-m@users.noreply.github.com>
2018-11-05 23:30:09 +0900

    work

--- abc113/d.py
+++ abc113/d.py
@@ -1,32 +1,30 @@
 # -*- coding: utf-8 -*-
 h,w,k = map(int, input().split())
+k -= 1
 mod = 1000000007
 
-cnt = {
-    -2: 0
-    ,-1: 0
-    ,0: 0
-    ,1: 2
-    ,2: 3
-    ,3: 5
-    ,4: 8
-    ,5: 13
-    ,6: 21
-    ,7: 33
-    ,8: 54
-}
+if w==1:
+    print(1)
+    exit()
 
 dp = [[0 for _ in range(w)] for _ in range(h+1)]
 dp[0][0] = 1
 
 for hi in range(1,h+1):
-    for wi in range(w):
-        if wi==0:
-            dp[hi][wi] = ((dp[hi-1][wi]+dp[hi-1][wi+1])*cnt[w-2])%mod
-        elif wi==w-1:
-            dp[hi][wi] = ((dp[hi-1][wi]+dp[hi-1][wi-1])*cnt[w-2])%mod
-        else:
-            dp[hi][wi] = ((dp[hi-1][wi]+dp[hi-1][wi-1]+dp[hi-1][wi+1])*cnt[wi-2]*cnt[w-wi-2])%mod
+    for b in range(2**(w-1)):
+        judge = False
+        for i in range(w-2):
+            if (b>>i)&3 == 3:
+                judge = True
+                break
+        if judge:
+            continue
 
-print(dp)
-print(dp[h][k-1])
+        perm = [i for i in range(w)]
+        for i in range(w-1):
+            if (b>>i)&1==1:
+                perm[i],perm[i+1] = perm[i+1],perm[i]
+        for i in range(w):
+            dp[hi][perm[i]]  = (dp[hi][perm[i]]+dp[hi-1][i])%mod
+
+print(dp[h][k])
