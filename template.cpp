f4ee2c3cb81487b357a99f2ba554da95d7b8ac85
MIKI Takushi <takushi-m@users.noreply.github.com>
2018-12-13 09:06:47 +0900

    work

--- template.cpp
+++ template.cpp
@@ -10,6 +10,12 @@ typedef ull unsigned long long;
 void sample(){
   vector<vector<int>> vvi = vector<vector<int>>(20, vector<int>(10, 0));  // vvi[20][10]
   vector<vector<vector<int>>> vvvi = vector<vector<vector<int>>>(30, vector<vector<int>>(20, vector<int>(10, 0)));  // vvvi[30][20][10]
+
+  vector<int> v = {3,2,1};
+  sort(v.begin(), v.end());
+  do {
+    // pass
+  } while(next_permutation(v.begin(), v.end()));
 }
 
 void main(){
