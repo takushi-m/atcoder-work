0e5017065472bbdf05083d68ee7ad628417f17c9
MIKI Takushi <takushi-m@users.noreply.github.com>
2018-12-26 23:17:24 +0900

    work

--- library.py
+++ library.py
@@ -135,3 +135,28 @@ def extGcd(a,b):
     y,x = extGcd(b, a%b)
     y -= a//b * x
     return x,y
+
+
+# 二分探索
+def lower_bound(al, key):
+    """
+    一般に左側が満たしておらず、右側は満たしている判定基準(境目は一つ)ならlower_boundになる
+    (条件を満たすもののうち、最小のインデックスを返す)
+    単調にさえなっていれば、左右が逆になっていても検索には使える(ng/okも一緒に逆にする必要がある)
+    """
+    def isOk(a):
+        # alの中でkey以上のアイテムのうち最小のインデックスを返す
+        return a>=key
+
+    ng = -1
+    ok = len(al)
+
+    while abs(ok-ng)>1:
+        mid = (ok+ng)//2
+
+        if isOk(al[mid]):
+            ok = mid
+        else:
+            ng = mid
+
+    return ok
