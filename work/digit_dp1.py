55b0ac56d88291d93c44e976962ff1c444a42584
MIKI Takushi <takushi-m@users.noreply.github.com>
2019-01-23 23:23:43 +0900

    work

---
+++ work/digit_dp1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+MOD = 10**9 + 7
+
+def f(n):
+    dp = [[0,0] for _ in range(len(n)+1)]
+    dp[0][0] = 1
+    for i in range(len(n)):
+        for j in range(2):
+            if j==1:
+                x = 9
+            else:
+                x = int(n[i])
+
+            for d in range(x+1):
+                if j==1 or d<x:
+                    dp[i+1][1] += dp[i][j]
+                else:
+                    dp[i+1][0] += dp[i][j]
+    print(dp)
+    return sum(dp[len(n)])
+
+print(f("100"))
