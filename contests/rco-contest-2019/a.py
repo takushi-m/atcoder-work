5b22e9b998402cc48070442da5e628fd9eb8acac
MIKI Takushi <takushi-m@users.noreply.github.com>
2019-02-12 09:08:50 +0900

    work

--- contests/rco-contest-2019/a.py
+++ contests/rco-contest-2019/a.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-from math import sqrt
-from random import randint, shuffle
+from math import sqrt, exp
+from random import randint, shuffle, random
 import time
 from pprint import pprint
-start = time.time()
-n = int(input())
+n = -1
 xy = []
 
 def dis(i,j):
@@ -13,68 +12,62 @@ def dis(i,j):
 def score(path):
     d = []
     for i in range(n-1):
-        d.append(dis(i,i+1))
-    d.append(dis(n-1,0))
+        d.append(dis(path[i],path[i+1]))
+    d.append(dis(path[n-1],path[0]))
     avg = sum(d)/n
     return sum([(d[i]-avg)**2 for i in range(n)])
 
-diff = 0
-table = [[[] for _ in range(4)] for _ in range(4)]
-for i in range(n):
-    x,y = map(int, input().split())
-    xy.append((x,y))
-    xi = x//126
-    yi = y//126
-    table[xi][yi].append(i)
-    for d in [[diff,0],[0,diff],[-diff,0],[0,-diff]]:
-        xi = (x+d[0])//126
-        yi = (y+d[1])//126
-        if 4>xi>=0 and 4>yi>=0:
-            table[xi][yi].append(i)
-
-used = [False for _ in range(n)]
-def pick(xi,yi,i):
-    if i<len(table[xi][yi]) and not used[table[xi][yi][i]]:
-        used[table[xi][yi][i]] = True
-        return table[xi][yi][i]
-
-res = []
-for i in range(n):
-    res.append(pick(1,1,i))
-    res.append(pick(0,1,i))
-    res.append(pick(0,0,i))
-    res.append(pick(1,0,i))
-    res.append(pick(2,0,i))
-    res.append(pick(3,0,i))
-    res.append(pick(3,1,i))
-    res.append(pick(3,2,i))
-    res.append(pick(3,3,i))
-    res.append(pick(2,3,i))
-    res.append(pick(1,3,i))
-    res.append(pick(0,3,i))
-    res.append(pick(0,2,i))
-    res.append(pick(1,2,i))
-    res.append(pick(2,2,i))
-    res.append(pick(2,1,i))
-
-tmp = []
-for p in res:
-    if p is not None:
-        tmp.append(p)
-res = tmp
-
-
-tmp = []
-for i in range(n):
-    if i==n-1:
-        tmp.append((i, dis(res[n-1],res[0])))
+def next_p(s,s2,itr,init):
+    if itr<1000:
+        beta = 0.1
     else:
-        tmp.append((i, dis(res[i],res[i+1])))
-tmp.sort()
+        beta = 0.1*init**itr
+    # print(beta)
+    if s>=s2:
+        return 1
+    else:
+        delta = s - s2
+        if delta*beta>100:
+            return 1
+        elif delta*beta<100:
+            return 0
+        else:
+            return exp(delta*beta)
 
-mini = tmp[0][0]
-maxi = tmp[-1][0]
-res[mini],res[maxi] = res[maxi],res[mini]
+def fun(init):
+    start = time.time()
+    res = [i for i in range(n)]
+    shuffle(res)
+    s = score(res)
+    idx = 1
+    while (time.time()-start)<1.95:
+        i = randint(0,199)
+        j = randint(0,199)
+        if abs(i-j)<=1:
+            continue
 
-for p in res:
-    print(p)
+        res[(i+1)%n],res[(j+1)%n] = res[(j+1)%n],res[(i+1)%n]
+        s2 = score(res)
+        if next_p(s,s2,idx,init)>random():
+            s = s2
+            # print(s)
+        else:
+            # 戻す
+            res[(i+1)%n],res[(j+1)%n] = res[(j+1)%n],res[(i+1)%n]
+        idx += 1
+    return s,res
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for _ in range(n):
+        x,y = map(int, input().split())
+        xy.append((x,y))
+
+
+    # init = 1.01
+    # for i in range(1,10+1):
+    #     print(init**i, fun(init**i)[0]/10**10)
+    s,res = fun(1.01)
+    for r in res:
+        print(r)
