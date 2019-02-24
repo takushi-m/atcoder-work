7f61c2102841aa5908513c8f85691d62e9524b94
MIKI Takushi <takushi-m@users.noreply.github.com>
2019-02-24 20:56:46 +0900

    work

--- work/dwango2017qual_b.py
+++ work/dwango2017qual_b.py
@@ -1,42 +1,21 @@
 # -*- coding: utf-8 -*-
-t = list(input())
+t = input()
 n = len(t)
-t2 = [t[i] for i in range(n)]
 
-for i in range(n):
-    if t[i]=="?":
-        if i==0:
-            t[i] = "2"
-        elif t[i-1]=="2":
-            t[i] = "5"
-        else:
-            t[i] = "2"
-res1 = 0
-i = 0
-while i<n:
-    j = i
-    while j<n-1 and t[j]=="2" and t[j+1]=="5":
-        j += 2
-    res1 = max(res1, j-i)
-    i = max(i+1,j)
+dp = [0 for _ in range(n+1)]
 
+for i in range(1,n):
+    tmp = 0
+    if t[i-1]=="2" and t[i]=="5":
+        tmp = 2
+    elif t[i-1]=="2" and t[i]=="?":
+        tmp = 2
+    elif t[i-1]=="?" and t[i]=="5":
+        tmp = 2
+    elif t[i-1]=="?" and t[i]=="?":
+        tmp = 2
 
+    if tmp>0:
+        dp[i] = dp[i-2] + 2
 
-for i in range(n-1,-1,-1):
-    if t2[i]=="?":
-        if i==n-1:
-            t2[i] = "5"
-        elif t2[i+1]=="2":
-            t2[i] = "5"
-        else:
-            t2[i] = "2"
-res2 = 0
-i = 0
-while i<n:
-    j = i
-    while j<n-1 and t2[j]=="2" and t2[j+1]=="5":
-        j += 2
-    res2 = max(res2, j-i)
-    i = max(i+1,j)
-
-print(max(res1,res2))
+print(max(dp))
