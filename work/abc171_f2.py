7bebf44ab063a7d3d5ecca20d4e18c780f851a98
MIKI Takushi <takushi-m@users.noreply.github.com>
2020-06-21 23:20:58 +0900

    work

---
+++ work/abc171_f2.py
@@ -0,0 +1,41 @@
+class BiCoeff(object):
+    def __init__(self, MAX, m):
+        super(BiCoeff, self).__init__()
+        fac = [0]*MAX
+        finv = [0]*MAX
+        inv = [0]*MAX
+
+        fac[0] = 1
+        fac[1] = 1
+        finv[0] = 1
+        finv[1] = 1
+        inv[1] = 1
+        for i in range(2,MAX):
+            fac[i] = (fac[i-1]*i)%m
+            inv[i] = m - (inv[m%i] * (m//i))%m
+            finv[i] = (finv[i-1] * inv[i])%m
+
+        self.MAX = MAX
+        self.m = m
+        self.fac = fac
+        self.finv = finv
+        self.inv = inv
+
+    def calc(self,n,k):
+        if n<k:
+            return 0
+        if n<0 or k<0:
+            return 0
+        return (self.fac[n] * (self.finv[k]*self.finv[n-k])%self.m)%self.m
+
+mod = 10**9 + 7
+k = int(input())
+s = input()
+n = len(s)
+
+b = BiCoeff(n+k+10, mod)
+res = 0
+for t in range(k+1):
+    res += b.calc(n+k - (t+1), k-t)*pow(25,k-t,mod)*pow(26,t,mod)
+    res %= mod
+print(res)
